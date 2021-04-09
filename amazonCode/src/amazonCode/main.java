0c8c30a16613a20ab5112c6b4f8501557bb07470
LucNguyen1 <77746629+LucNguyen1@users.noreply.github.com>
2021-04-08 20:51:52 -0400

    this is the changes done in java
    the commit and push is scuff, lets see you try to copy and paste the code on your own and see if you can commit and push.
    
    we even might have to ask the professor on how to do it

--- amazonCode/src/amazonCode/main.java
+++ amazonCode/src/amazonCode/main.java
@@ -4,7 +4,43 @@ public class main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+ String Username;
+        String Password;
+        
+        
+//set password and username, figuring out to let the user have more freedom and the username and password they create
+        Password = "lord";
+        Username = "Jesus";
+
+        
+        //entering the username
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("Enter Username : ");
+        String username = input1.next();
+
+        //password
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("Enter Password : ");
+        String password = input2.next();
+
+        
+        //still determining on how let the user more freedom
+        if (username.equals(Username) && password.equals(Password)) {
+
+            System.out.println("Access Granted! Welcome!");
+        }
+
+        else if (username.equals(Username)) {
+            System.out.println("Invalid Password!");
+        } else if (password.equals(Password)) {
+            System.out.println("Invalid Username!");
+        } else {
+            System.out.println("Invalid Username & Password!");
+        }
+
+    }
+    }
 
     }
 
-}
+
